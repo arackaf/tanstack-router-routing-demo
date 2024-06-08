95b26106ccba8d51eb4d03c8b0e30a3ff7e067f6
Adam Rackis <adamrackis@hotmail.com>
2024-06-08 16:43:48 -0500

    started

---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // ...,
+    TanStackRouterVite(),
+  ],
+});
