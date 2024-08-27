4a848f4173511d78bf0ec5a178f5d0cb715392b5
Adam Rackis <adamrackis@hotmail.com>
2024-08-26 20:35:51 -0500

    Add loader

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,27 @@
 import React from "react";
-import { createFileRoute, createLazyFileRoute } from "@tanstack/react-router";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { useData } from "../useData";
 
 export const Route = createFileRoute("/")({
   component: Index,
-  loader: () => {
+  loader: async () => {
     console.log("INDEX LOADER");
-    return 12;
+    await new Promise((res) => setTimeout(res, 1500));
+    return { b: Math.random() };
   },
+  staleTime: 15000,
+  gcTime: 15000,
 });
 
 function Index() {
   const { data } = useData();
+  const loaderData = Route.useLoaderData();
+
   return (
     <div className="p-2">
       <h3 className="text-red-500">Welcome Home!</h3>
       <p>{JSON.stringify(data ?? {})}</p>
+      <p>{JSON.stringify(loaderData ?? {})}</p>
     </div>
   );
 }
